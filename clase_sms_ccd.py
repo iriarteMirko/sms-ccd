36fa5d2208621064cc952bd8abbe48d0ddaa2fb2
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-04-15 16:00:11 -0500

    initial commit

---
+++ clase_sms_ccd.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import shutil
+import os
+
+class Clase_SMS:
+    def __init__(self, fecha_hoy, fecha_ayer, fecha_hoy_txt, ruta_zfir60, ruta_modelo, ruta_dacxanalista):
+        self.fecha_hoy = fecha_hoy
+        self.fecha_ayer = fecha_ayer
+        self.fecha_hoy_txt = fecha_hoy_txt
+        
+        self.ruta_zfir60 = ruta_zfir60 + "/ZFIR60_" + self.fecha_hoy + ".xlsx"
+        self.ruta_modelo = ruta_modelo + "/Modelo de Evaluación de Pedidos de Equipos_" + self.fecha_hoy + ".xlsx"
+        
+        self.fbl5n = "./bases/FBL5N.xlsx"
+        self.reporte_recaudacion = "./bases/Reporte_Recaudacion_" + self.fecha_hoy + ".csv"
+        self.deudores_sap = "./bases/Deudores_SAP.xlsx"
+        
+        self.modelo = "./modelo_zfir_recaudacion/Modelo de Evaluación de Pedidos de Equipos_" + self.fecha_hoy + ".xlsx"
+        self.zfir = "./modelo_zfir_recaudacion/Zfir" + self.fecha_hoy + ".xlsx"
+        self.recaudacion = "./modelo_zfir_recaudacion/Recaudacion_" + self.fecha_hoy + ".xlsx"
+        
+        self.ruta_dacxanalista = ruta_dacxanalista
+        self.ruta_base_celulares = "./bases/Base_Celulares_CCD.xlsx"
+        
+        self.ld = "./archivos txt/LD " + self.fecha_hoy_txt + ".txt"
+        self.nivel_1 = "./archivos txt/Nivel 1 " + self.fecha_hoy_txt + ".txt"
+
+    def abrir_reporte(self, ruta):
+        os.startfile(ruta)
+
+    def traer_archivos(self):
+        shutil.copy(self.ruta_modelo, self.modelo)
+
+    def preparar_zfir60(self):
+        df_zfir60 = pd.read_excel(self.ruta_zfir60, sheet_name="Sheet1")
+        df_zfir60 = df_zfir60[["Cliente Pa", "Total Vencida"]]
+        df_zfir60["Total Vencida"] = df_zfir60["Total Vencida"].clip(lower=0)
+        df_zfir60 = df_zfir60.groupby("Cliente Pa")["Total Vencida"].sum().reset_index()
+        df_zfir60.to_excel(self.zfir, sheet_name="BASE", index=False)
+
+    def exportar_deudores_sap(self):
+        global df_recaudacion
+        df_recaudacion = pd.read_csv(self.reporte_recaudacion, encoding="latin1")
+        df_recaudacion = df_recaudacion.drop("USER_ID", axis=1)
+        df_recaudacion = df_recaudacion[df_recaudacion["FECHA_RECAUDACION"] == int(self.fecha_ayer)]
+        df_recaudacion[["SAP"]].to_excel(self.deudores_sap, index=False)
+        self.abrir_reporte(self.deudores_sap)
+
+    def preparar_fbl5n(self):
+        global df_fbl5n
+        df_fbl5n = pd.read_excel(self.fbl5n)
+        df_fbl5n = df_fbl5n[df_fbl5n["Cuenta"].notna()]
+        df_fbl5n = df_fbl5n[df_fbl5n["ACC"] == "PE07"]
+        df_fbl5n = df_fbl5n[["Cuenta","Importe en ML"]]
+        df_fbl5n["Importe en ML"] = df_fbl5n["Importe en ML"] * -1
+        df_fbl5n = df_fbl5n.groupby("Cuenta")["Importe en ML"].sum().reset_index()
+
+    def preparar_recaudacion(self):
+        df_fbl5n.set_index("Cuenta", inplace=True)
+        df_recaudacion["FALTA"] = df_recaudacion["SAP"].map(df_fbl5n["Importe en ML"])
+        df_recaudacion["FALTA"] = df_recaudacion["FALTA"].fillna(0)
+        df_fbl5n.reset_index(inplace=True)
+        df_recaudacion["RESTA"] = df_recaudacion["LIMITE_CREDITICIO"] - df_recaudacion["SALDO_ACTUAL"]
+        df_recaudacion["TOTAL"] = df_recaudacion["FALTA"] + df_recaudacion["RESTA"]
+        df_recaudacion.to_excel(self.recaudacion, sheet_name="BASE", index=False)
+
+    def exportar_archivos_txt(self):
+        pass
---
+++ sms_ccd.py
@@ -0,0 +1,166 @@
+from clase_sms_ccd import Clase_SMS
+from datetime import datetime, timedelta
+from tkinter import messagebox
+from customtkinter import *
+import pandas as pd
+import warnings
+import threading
+import time
+
+warnings.filterwarnings("ignore")
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS2
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+def main():
+    
+    def salir():
+        app.destroy()
+
+    def verificar_thread(thread):
+        if thread.is_alive():
+            app.after(1000, verificar_thread, thread)
+        else:
+            boton1.configure(state="normal")
+            boton2.configure(state="normal")
+            boton3.configure(state="normal")
+            boton4.configure(state="normal")
+            boton_salir.configure(state="normal")
+
+    def iniciar_proceso(accion):
+        if accion == 1:
+            thread = threading.Thread(target=accion_boton1)
+        elif accion == 2:
+            thread = threading.Thread(target=accion_boton2)
+        elif accion == 3:
+            thread = threading.Thread(target=accion_boton3)
+        elif accion == 4:
+            thread = threading.Thread(target=accion_boton4)
+        else:
+            pass
+        boton1.configure(state="disabled")
+        boton2.configure(state="disabled")
+        boton3.configure(state="disabled")
+        boton4.configure(state="disabled")
+        boton_salir.configure(state="disabled")
+        
+        thread.start()
+        app.after(1000, verificar_thread, thread)
+
+    def accion_boton1():
+        progressbar.start()
+        try:
+            reporte.traer_archivos()
+            reporte.preparar_zfir60()
+            messagebox.showinfo("PASO 1", "COMPLETADO")
+        except Exception as e:
+            messagebox.showerror("ERROR", "Algo salió mal. Por favor, intente nuevamente.\nDetalles: " + str(e))
+        finally:
+            progressbar.stop()
+
+    def accion_boton2():
+        progressbar.start()
+        try:
+            reporte.exportar_deudores_sap()
+        except Exception as e:
+            messagebox.showerror("ERROR", "Algo salió mal. Por favor, intente nuevamente.\nDetalles: " + str(e))
+        finally:
+            progressbar.stop()
+
+    def accion_boton3():
+        progressbar.start()
+        try:
+            reporte.preparar_fbl5n()
+            reporte.preparar_recaudacion()
+            messagebox.showinfo("PASO 2", "COMPLETADO")
+        except Exception as e:
+            messagebox.showerror("ERROR", "Algo salió mal. Por favor, intente nuevamente.\nDetalles: " + str(e))
+        finally:
+            progressbar.stop()
+
+    def accion_boton4():
+        progressbar.start()
+        try:
+            reporte.exportar_archivos_txt()
+            messagebox.showinfo("SMS C&CD", "MENSAJES LISTOS!")
+        except Exception as e:
+            messagebox.showerror("ERROR", "Algo salió mal. Por favor, intente nuevamente.\nDetalles: " + str(e))
+        finally:
+            progressbar.stop()
+
+    def generar_reporte():
+        global reporte
+        excel_rutas = resource_path("./RUTAS.xlsx")
+        df_rutas = pd.read_excel(excel_rutas)
+        fecha_hoy = datetime.today()
+        fecha_ayer = fecha_hoy - timedelta(days=1)
+        fecha_ayer = fecha_ayer.strftime("%Y%m%d")
+        fecha_hoy = datetime.today().strftime("%Y%m%d")
+        fecha_hoy_txt = datetime.today().strftime("%d.%m.%Y")
+        ruta_zfir60 = df_rutas["RUTA"][0]
+        ruta_modelo = df_rutas["RUTA"][1]
+        ruta_dacxanalista = df_rutas["RUTA"][2]
+        
+        reporte = Clase_SMS(fecha_hoy, fecha_ayer, fecha_hoy_txt, ruta_zfir60, ruta_modelo, ruta_dacxanalista)
+
+    def crear_app():
+        global app, boton1, boton2, boton3, boton4, progressbar, boton_salir
+        generar_reporte()
+        app = CTk()
+        app.title("SMS C&CD")
+        app.iconbitmap(resource_path("./images/icono.ico"))
+        app.resizable(False, False)
+        set_appearance_mode("light")
+        
+        main_frame = CTkFrame(app)
+        main_frame.pack_propagate(0)
+        main_frame.pack(fill="both", expand=True)
+        
+        frame_title = CTkFrame(main_frame)
+        frame_title.grid(row=0, column=0, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        
+        titulo = CTkLabel(frame_title, text="Mensajes de Texto C&CD", font=("Arial",25,"bold"))
+        titulo.pack(fill="both", expand=True, padx=(20,20), ipady=20, anchor="center")
+        
+        frame_botones = CTkFrame(main_frame)
+        frame_botones.grid(row=1, column=0, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        
+        boton1 = CTkButton(frame_botones, text=">>> PASO 1 <<<", font=("Calibri",17), text_color="black", 
+                            fg_color="transparent", border_color="black", border_width=3, hover_color="#d11515", 
+                            width=25, corner_radius=10, command=lambda: iniciar_proceso(1))
+        boton1.pack(anchor="center", fill="both", expand=True, ipady=10, padx=(20, 20), pady=(20, 0))
+        
+        boton2 = CTkButton(frame_botones, text="EXPORTAR DEUDORES SAP", font=("Calibri",17), text_color="black", 
+                            fg_color="transparent", border_color="black", border_width=3, hover_color="#d11515", 
+                            width=25, corner_radius=10, command=lambda: iniciar_proceso(2))
+        boton2.pack(anchor="center", fill="both", expand=True, ipady=10, padx=(20, 20), pady=(20, 0))
+        
+        boton3 = CTkButton(frame_botones, text=">>> PASO 2 <<<", font=("Calibri",17), text_color="black", 
+                            fg_color="transparent", border_color="black", border_width=3, hover_color="#d11515", 
+                            width=25, corner_radius=10, command=lambda: iniciar_proceso(3))
+        boton3.pack(anchor="center", fill="both", expand=True, ipady=10, padx=(20, 20), pady=(20, 0))
+        
+        boton4 = CTkButton(frame_botones, text="EXPORTAR ARCHIVOS TXT", font=("Calibri",17), text_color="black", 
+                            fg_color="transparent", border_color="black", border_width=3, hover_color="#d11515", 
+                            width=25, corner_radius=10, command=lambda: iniciar_proceso(4))
+        boton4.pack(anchor="center", fill="both", expand=True, ipady=10, padx=(20, 20), pady=(20, 20))
+        
+        progressbar = CTkProgressBar(main_frame, mode="indeterminate", orientation="horizontal", 
+                                        progress_color="#d11515", height=10, border_width=0)
+        progressbar.grid(row=2, column=0, padx=(20, 20), pady=(10, 0), sticky="nsew")
+        
+        boton_salir = CTkButton(main_frame, text="SALIR", font=("Calibri",17), text_color="black", 
+                                fg_color="transparent", border_color="black", border_width=3, hover_color="#d11515", 
+                                width=10, corner_radius=10, command=salir)
+        boton_salir.grid(row=3, column=0, padx=(150, 150), pady=(20, 20), sticky="nsew")
+        
+        app.mainloop()
+
+    crear_app()
+
+if __name__ == "__main__":
+    main()

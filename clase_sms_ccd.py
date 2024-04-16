54cefd53d2b7968a235a7781f28504a573ec9834
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-04-16 17:26:39 -0500

    update class_sms_ccd

--- clase_sms_ccd.py
+++ clase_sms_ccd.py
@@ -33,31 +33,32 @@ class Clase_SMS:
         df_zfir60["Total Vencida"] = df_zfir60["Total Vencida"].clip(lower=0)
         df_zfir60 = df_zfir60.groupby("Cliente Pa")["Total Vencida"].sum().reset_index()
         df_zfir60.to_excel(self.zfir, sheet_name="BASE", index=False)
+        self.df_zfir60 = df_zfir60
 
     def exportar_deudores(self):
-        global df_recaudacion
         df_recaudacion = pd.read_csv(self.reporte_recaudacion, encoding="latin1")
         df_recaudacion = df_recaudacion.drop("USER_ID", axis=1)
         df_recaudacion = df_recaudacion[df_recaudacion["FECHA_RECAUDACION"] == int(self.fecha_ayer)]
         df_recaudacion[["SAP"]].to_excel(self.deudores, index=False)
+        self.df_recaudacion = df_recaudacion
 
     def preparar_fbl5n(self):
-        global df_fbl5n
         df_fbl5n = pd.read_excel(self.fbl5n)
         df_fbl5n = df_fbl5n[df_fbl5n["Cuenta"].notna()]
         df_fbl5n = df_fbl5n[df_fbl5n["ACC"] == "PE07"]
         df_fbl5n = df_fbl5n[["Cuenta","Importe en ML"]]
         df_fbl5n["Importe en ML"] = df_fbl5n["Importe en ML"] * -1
         df_fbl5n = df_fbl5n.groupby("Cuenta")["Importe en ML"].sum().reset_index()
+        self.df_fbl5n = df_fbl5n
 
     def preparar_recaudacion(self):
-        df_fbl5n.set_index("Cuenta", inplace=True)
-        df_recaudacion["FALTA"] = df_recaudacion["SAP"].map(df_fbl5n["Importe en ML"])
-        df_recaudacion["FALTA"] = df_recaudacion["FALTA"].fillna(0)
-        df_fbl5n.reset_index(inplace=True)
-        df_recaudacion["RESTA"] = df_recaudacion["LIMITE_CREDITICIO"] - df_recaudacion["SALDO_ACTUAL"]
-        df_recaudacion["TOTAL"] = df_recaudacion["FALTA"] + df_recaudacion["RESTA"]
-        df_recaudacion.to_excel(self.recaudacion, sheet_name="BASE", index=False)
+        self.df_fbl5n.set_index("Cuenta", inplace=True)
+        self.df_recaudacion["FALTA"] = self.df_recaudacion["SAP"].map(self.df_fbl5n["Importe en ML"])
+        self.df_recaudacion["FALTA"] = self.df_recaudacion["FALTA"].fillna(0)
+        self.df_fbl5n.reset_index(inplace=True)
+        self.df_recaudacion["RESTA"] = self.df_recaudacion["LIMITE_CREDITICIO"] - self.df_recaudacion["SALDO_ACTUAL"]
+        self.df_recaudacion["TOTAL"] = self.df_recaudacion["FALTA"] + self.df_recaudacion["RESTA"]
+        self.df_recaudacion.to_excel(self.recaudacion, sheet_name="BASE", index=False)
 
     def exportar_archivos_txt(self):
         pass
